243eec957fa39866c9ff4a69e0080229a21787b0
baptiste-boimard <bouketin27@gmail.com>
2023-03-10 13:55:53 +0100

    add response and error in login

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ import { SUBMIT_LOGIN } from '../actions/login';
 
 /** Instance of AXIOS with options */
 const instance = axios.create({
-  baseURL:'',
+  baseURL:'http://localhost:3333',
 });
 
 const auth = (store) => (next) => (action) => {
@@ -13,15 +13,19 @@ const auth = (store) => (next) => (action) => {
 
     /**Submit login to API login */
     case SUBMIT_LOGIN:
+      console.log(action.email, action.password);
+
       instance.post('/login', {
         email: action.email,
         password: action.password,
       })
         .then((response) => {
           //action en cas de réponse
+          console.log(response);
         })
         .catch((error) => {
           //action en cas d'erreur
+          console.log(error);
         });
       break;
     default:
